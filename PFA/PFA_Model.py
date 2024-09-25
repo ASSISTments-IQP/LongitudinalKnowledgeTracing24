1476ee7dc54569aed1ebebc7b01cd7a0dad419c6
Morgan Lee <mplee@wpi.edu>
2024-09-25 12:13:46 -0400

    Model file to handle all the stuff

---
+++ PFA/PFA_Model.py
@@ -0,0 +1,100 @@
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import log_loss, roc_auc_score
+import scipy.sparse as sp
+import numpy as np
+from tqdm import tqdm
+
+
+class PFA:
+    def __init__(self, verbose):
+        self.skills = []
+        self.n_s = len(self.skills)
+        self.verbose = verbose
+        self.model = LogisticRegression(penalty=None, solver='lbfgs', verbose=verbose, max_iter=10 ** 3)
+
+
+    def preprocess(self, data):
+        # Create wins & fails aggregators & make sure data is sorted correctly
+        wins = []
+        fails = []
+        data.sort_values(by=['user_xid','skill_id','start_time'], inplace=True)
+        gby = data.groupby(by=['user_xid', 'skill_id'])
+
+        # Loop (w/ tqdm if verbose) to create wins & fails features
+        for name, group in (tqdm(gby), gby)[self.verbose > 0]:
+            w = 0
+            f = 0
+            for idx, row in group.iterrows():
+                wins.append(w)
+                fails.append(f)
+                if row['discrete_score'] == 1:
+                    w += 1
+                else:
+                    f += 1
+
+        data['wins'] = wins
+        data['fails'] = fails
+
+
+        # Start creating the sparse array for the data
+        res_arr = sp.dok_array((data.shape[0], 3*(self.n_s+1)))
+        i = 0
+        it = data.iterrows()
+
+
+        for idx, row in (tqdm(it), it)[self.verbose > 0]:
+            if row['skill_id'] in self.skills:
+                skill_idx = np.where(self.skills == row['skill_id'])[0][0]
+            else:
+                skill_idx = self.n_s + 1
+
+            res_arr[i,skill_idx] = 1
+            res_arr[i,self.n_s+1+skill_idx] = row['wins']
+            res_arr[i,2*(self.n_s+1)+skill_idx] = row['fails']
+            i += 1
+
+        X = res_arr.tocoo()
+        y = data.discrete_score.to_numpy(copy=True)
+
+        return X, y
+
+
+    def fit(self, data):
+        self.skills = np.sort(data.skill_id.unique())
+        self.n_s = len(self.skills)
+
+        print("Beginning data preprocessing")
+        X, y = self.preprocess(data)
+        print("Data preprocessing finished, beginning fitting.")
+
+        self.model.fit(X,y)
+
+        print("Model Training finished, final statistics:")
+        y_pred = self.model.predict_proba(X)[:,1]
+
+        ll = log_loss(y,y_pred)
+        auc = roc_auc_score(y,y_pred)
+
+        print(f"Training loss: {ll}")
+        print(f"Training AUC: {auc}")
+
+        return ll, auc
+
+
+    def eval(self, data):
+        if self.n_s == 0:
+            print("No model has been trained, aborting")
+            return
+
+        print("Beginning data preprocessing")
+        X, y = self.preprocess(data)
+
+        y_pred = self.model.predict_proba(X)[:, 1]
+
+        ll = log_loss(y, y_pred)
+        auc = roc_auc_score(y, y_pred)
+
+        print(f"Log loss: {ll}")
+        print(f"AUC: {auc}")
+
+        return ll, auc
