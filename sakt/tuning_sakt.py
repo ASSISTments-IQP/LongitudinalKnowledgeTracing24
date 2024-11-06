fc6cb3181f8b5ad13cb29c183aa72235afc18d8b
afrenk <artem.frenk@assistments.org>
2024-11-06 16:00:52 -0500

    added torch beginnings

--- sakt/tuning_sakt.py
+++ sakt/tuning_sakt.py
@@ -6,12 +6,12 @@ train_df = pd.read_csv('../Data/samples/23-24/sample3.csv')
 val_df = pd.read_csv('../Data/samples/23-24/sample4.csv')
 def objective(trial):
     num_steps = trial.suggest_int('num_steps', 20, 100, step = 10),
-    batch_size = trial.suggest_categorical('batch_size', [16, 32, 64]),
+    batch_size = trial.suggest_categorical('batch_size', 16, 64, step = 16),
     d_model = trial.suggest_int('d_model', 128, 512, step = 32),
     num_heads = trial.suggest_int('num_heads', 2, 16, step = 2),
     dropout_rate = trial.suggest_int('dropout_rate', 0.1, 0.5)
     
-    model = SAKTmodel(
+    model = SAKTModel(
         num_steps = num_steps,
         batch_size = batch_size,
         d_model = d_model,
