025b77c04a9745a76d77a85a9d07c10c5f3ed721
Artem Frenk <afrenk@wpi.edu>
2024-11-12 01:30:14 -0500

    added script to test offset to gpu

--- DKT/run_test.sh
+++ DKT/run_test.sh
@@ -3,10 +3,10 @@
 #SBATCH -n 1
 #SBATCH --mem=128g
 #SBATCH -J "DKTHPOPT"
-#SBATCH -p short
+#SBATCH -p academic
 #SBATCH -t 01:00
-#SBATCH --gres=gpu:2
+#SBATCH --gres=gpu:1
 module load python/3.12.7
 module load cuda
-source ~/myenvs/lkt-env/bin/activate
-python3 ./gpu_cuda_test.py
+source cuda/bin/activate
+python3 ./test_share.py
