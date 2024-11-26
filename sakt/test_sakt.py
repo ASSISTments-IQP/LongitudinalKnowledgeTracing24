2b56009f6e1594fc9d6c2b9166316d7bf3c37d48
patrick-lee22 <prlee@wpi.edu>
2024-11-26 11:51:33 -0500

    Lets test pt sakt on turing

--- sakt/test_sakt.py
+++ sakt/test_sakt.py
@@ -7,5 +7,5 @@ train_data = pd.read_csv('../samples/19-20/sample1.csv')
 
 
 
-mod = SAKTModel(num_steps=50, batch_size = 32, d_model=128, num_heads=8, dropout_rate=0.2, init_learning_rate=1e-3, learning_decay_rate=0.98,)
-mod.fit(train_data, num_epochs=1)
+mod = SAKTModel(num_steps=50, batch_size = 32, d_model=128, num_heads=8, dropout_rate=0.2, init_learning_rate=1e-3, learning_decay_rate=0.98)
+mod.fit(train_data, num_epochs=2)
