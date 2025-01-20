5d6ec718930891abc9a86d48ab3e029e38e24cbb
patrick-lee22 <prlee@wpi.edu>
2025-01-20 13:52:28 -0500

    right ones

--- cross_year_deep_temp.sh
+++ cross_year_deep_temp.sh
@@ -6,7 +6,7 @@
 #SBATCH -p short
 #SBATCH -t 24:00:00
 #SBATCH --gres=gpu:10
-module load python
+module load python/3.12.7
 module load cuda
 source ~/myenvs/cuda-torch/bin/activate
 python3 ../run_cy_one_year.py MODEL_TYPE YEAR
--- within_year_deep_temp.sh
+++ within_year_deep_temp.sh
@@ -6,7 +6,7 @@
 #SBATCH -p short
 #SBATCH -t 24:00:00
 #SBATCH --gres=gpu:5
-module load python
+module load python/3.12.7
 module load cuda
 source ~/myenvs/cuda-torch/bin/activate
 python3 ../run_wy_one_year.py MODEL_TYPE YEAR
