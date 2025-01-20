dfb21bc21a333fbe4652caa9be7b6f6f6e6a0283
patrick-lee22 <prlee@wpi.edu>
2025-01-20 13:57:30 -0500

    add specific cuda version

--- cross_year_deep_temp.sh
+++ cross_year_deep_temp.sh
@@ -7,6 +7,6 @@
 #SBATCH -t 24:00:00
 #SBATCH --gres=gpu:10
 module load python/3.12.7
-module load cuda
+module load cuda/11.8.0
 source ~/myenvs/cuda-torch/bin/activate
 python3 ../run_cy_one_year.py MODEL_TYPE YEAR
--- within_year_deep_temp.sh
+++ within_year_deep_temp.sh
@@ -7,6 +7,6 @@
 #SBATCH -t 24:00:00
 #SBATCH --gres=gpu:5
 module load python/3.12.7
-module load cuda
+module load cuda/11.8.0
 source ~/myenvs/cuda-torch/bin/activate
 python3 ../run_wy_one_year.py MODEL_TYPE YEAR
