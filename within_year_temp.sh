5d009d89699499a43a1ae46bf756528f25d04494
patrick-lee22 <prlee@wpi.edu>
2025-01-22 14:32:31 -0500

    beefiest gpu

--- within_year_temp.sh
+++ within_year_temp.sh
@@ -6,7 +6,7 @@
 #SBATCH -p short
 #SBATCH -t 24:00:00
 #SBATCH --gres=gpu:1
-#SBATCH -C "A100|H100|H200"
+#SBATCH -C "H200"
 module load cuda
 source ~/myenvs/cuda-torch/bin/activate
 python3 ../run_deep_model_within_year_one_fold.py MODEL_TYPE YEAR FOLD_NUM
