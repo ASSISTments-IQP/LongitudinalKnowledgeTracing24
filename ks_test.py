f6575144c5c16263f9607bd6bc77a0951a26afae
Morgan Lee <mplee@wpi.edu>
2025-09-03 10:50:02 -0400

    variable not defined yet oops

--- ks_test.py
+++ ks_test.py
@@ -74,7 +74,7 @@ if __name__ == "__main__":
     pvals = []
     ks_stats = []
     for y in years:
-        for train_samp in tqdm(sample_nums, desc=f'WY {y}-{str(train_samp)}'):
+        for train_samp in tqdm(sample_nums, desc=f'WY {y}'):
             train = []
             test = []
             for s in sample_nums:
@@ -95,7 +95,7 @@ if __name__ == "__main__":
         for test_y_idx in range(train_y_idx,5):
             test_year = years[test_y_idx]
             for s in tqdm(sample_nums,desc=f'CY {train_year}-{test_year}-{str(s)}'):
-                cur_pval, cur_ks = test_distributional_similarity(sample_dict[train_year][s],sample_dict[test_year][s], fname=f'{train_year}-{test_year}-{str(s)}')
+                cur_pval, cur_ks = test_distributional_similarity(sample_dict[train_year][s],sample_dict[test_year][s], fname=f'{train_year}-{test_year}')
                 pvals.append(cur_pval)
                 ks_stats.append(cur_ks)
 
